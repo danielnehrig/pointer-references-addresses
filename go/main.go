5fe6b16f1b605176c80080648c9619abdd012d72
Daniel Nehrig <daniel.nehrig@redteclab.com>
2021-03-09 14:26:13 +0100

    fix go

--- go/main.go
+++ go/main.go
@@ -4,6 +4,7 @@
 //
 // Distributed under terms of the MIT license.
 //
+// Pass by reference Tutorial
 
 package main
 
@@ -13,13 +14,29 @@ type Object struct {
   value int
 }
 
+func mutateVar(value *int) {
+  *value = 10
+}
+
+func mutateObj(obj *Object) {
+  obj.value = 20
+}
+
 func main() {
   // declare init vars and complex data type
-  value = 5
-  obj = Object{
+  value := 5
+  obj := Object{
     value: 10,
   }
 
   // print types
-  fmt.println(value, obj.value)
+  fmt.Println("Print init vars")
+  fmt.Println(value, obj.value)
+
+  fmt.Println("Mutate vars and data struct")
+  mutateVar(&value)
+  mutateObj(&obj)
+
+  fmt.Println("Print mutated vars")
+  fmt.Println(value, obj.value)
 }
